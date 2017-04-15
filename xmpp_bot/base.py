b2c6f7fbca0b4b8f8fc95ce67778938eda545e59
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-04-15 23:05:14 +0200

    Create base chat bot class

---
+++ xmpp_bot/base.py
@@ -0,0 +1,25 @@
+from sleekxmpp import ClientXMPP
+
+class BaseBot(ClientXMPP):
+    def __init__(self, jid, password, nick, room):
+        ClientXMPP.__init__(self, jid, password)
+
+        self.nick = nick
+        self.room = room
+
+        self.register_plugin('xep_0045')
+
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("groupchat_message", self.groupchat_message)
+
+
+    def session_start(self, event):
+        self.get_roster()
+        self.send_presence()
+        self.plugin['xep_0045'].joinMUC(self.room,
+                                        self.nick,
+                                        wait=True)
+
+    def groupchat_message(self, msg):
+        if msg['mucnick'] != self.nick:
+            self.handle_message(msg)
